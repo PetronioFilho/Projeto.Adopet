e8ea35fcf6c8af81f022d2d8f06d70f504800a28
PetronioFilho <camposfilho84@gmail.com>
2025-12-17 18:16:19 -0300

    Segundo commit

---
+++ Alura.Adopet.Console/DocComando.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    internal class DocComando : System.Attribute
+    {
+        public DocComando(string instrucao, string documentacao)
+        {
+            Instrucao = instrucao;
+            Documentacao = documentacao;
+        }
+
+        public string Instrucao { get; set; }
+        public string Documentacao { get; set; }
+    }
+}
