f4cbde1d16aab98a8dc0866e6a2b5f77fd1a0ea7
PetronioFilho <camposfilho84@gmail.com>
2026-01-13 17:38:37 -0300

    Finalização do curso

--- Alura.Adopet.Console/Util/PetAPartirDoCsv.cs
+++ Alura.Adopet.Console/Util/PetAPartirDoCsv.cs
@@ -2,16 +2,32 @@
 
 namespace Alura.Adopet.Console.Util
 {
-    public class PetAPartirDoCsv
+    public static class PetAPartirDoCsv
     {
-        public Pet ConvertTexto(string linha)
-        {            
+        public static Pet ConverteDoTexto(this string linha)
+        {
+            if(!ValidaLinha(linha) || !ValidaGuid(linha))
+                return null;
             string[]? propriedades = linha.Split(';');
-            
+
             return new Pet(Guid.Parse(propriedades[0]),
             propriedades[1],
             int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro
             );
+        }
+
+        private static bool ValidaLinha(string linha)
+        {
+            if(string.IsNullOrEmpty(linha) || linha.Split(';').Count() != 3)
+                return false;  
+            return true;
+        }
+
+        private static bool ValidaGuid(string linha)
+        {
+            if(!Guid.TryParse(linha.Split(';')[0], out Guid guidResult))
+                return false;
+            return true;
         }
     }
 }
--- Alura.Adopet.Testes/PetAPartirDoCsvTeste.cs
+++ Alura.Adopet.Testes/PetAPartirDoCsvTeste.cs
@@ -9,14 +9,78 @@ namespace Alura.Adopet.Testes
         public void QuandoStringForValidaDEveRetornarUmPet()
         {
             //Arrange
-            string linha = "3fa85f64-5717-4562-b3fc-2c963f66afa6;Lima Limão;1";
-            var conversor = new PetAPartirDoCsv();
+            string linha = "3fa85f64-5717-4562-b3fc-2c963f66afa6;Lima Limão;1";            
 
             //Act   
-            Pet pet = conversor.ConvertTexto(linha);
+            Pet pet = linha.ConverteDoTexto();
 
             //Asset
             Assert.NotNull(pet);
+        }
+
+        [Fact]
+        public void QuandoStringForNula()
+        {
+            //Arrange
+            string linha = null;
+
+            //Act   
+            Pet pet = linha.ConverteDoTexto();
+
+            //Asset
+            Assert.Null(pet);
+        }
+
+        [Fact]
+        public void QuandoStringForVazia()
+        {
+            //Arrange
+            string linha = "";
+
+            //Act   
+            Pet pet = linha.ConverteDoTexto();
+
+            //Asset
+            Assert.Null(pet);
+        }
+
+        [Fact]
+        public void QuandoAStringNaoEstiverCompletada()
+        {
+            //Arrange
+            string linha = "3fa85f64-5717-4562-b3fc-2c963f66afa6;Lima Limão";
+
+            //Act   
+            Pet pet = linha.ConverteDoTexto();
+
+            //Asset
+            Assert.Null(pet);
+        }
+
+        [Fact]
+        public void QuandoAStringForMaiorQueOEstabelecido()
+        {
+            //Arrange
+            string linha = "3fa85f64-5717-4562-b3fc-2c963f66afa6;Lima Limão;1;1;1";
+
+            //Act   
+            Pet pet = linha.ConverteDoTexto();
+
+            //Asset
+            Assert.Null(pet);
+        }
+
+        [Fact]
+        public void QuandoOGuidForInvalido()
+        {
+            //Arrange
+            string linha = "'';Lima Limão;1";
+
+            //Act   
+            Pet pet = linha.ConverteDoTexto();
+
+            //Asset
+            Assert.Null(pet);
         }
     }
 }
